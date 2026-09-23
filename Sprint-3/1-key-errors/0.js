8098b0edfa9ad544812ee8cbd2e6b34f670e0a62
chefmatthaus-hub <chef.matthaus@gmail.com>
2026-09-23 17:09:07 +0200

    fix: correct syntax error in capitalise function and update comments

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -1,13 +1,22 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  It will make the first letter in the string input capitalised.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+// "SyntaxError: Identifier 'str' has already been declared"
+
+// capitalise("matthaus enjoys foood.")
+
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+
+// The error is thrown by declaring the 'str' variable again in the function with 'let' which causes it throw the SyntaxError, as it is already been declared.
+
+capitalise("matthaus enjoys food")
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  return '${str[0].toUpperCase()}${str.slice(1)}';
 }
-
-// =============> write your explanation here
-// =============> write your new code here
+console.log(return);
