5c410d89017a883e9f93487a463ba763c9868684
chefmatthaus-hub <chef.matthaus@gmail.com>
2026-09-23 18:56:51 +0200

    fix: update comments and remove redundant variable declaration in convertToPercentage function

--- Sprint-3/1-key-errors/1.js
+++ Sprint-3/1-key-errors/1.js
@@ -1,20 +1,30 @@
 // Predict and explain first...
+// It will give you a percentage of 50% using the function by multiplying the 'decimalNumber' by 100 for the 'percentage' variables.  
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// It will be a syntaxError because the 'decimalNumber' is already called in the function parameter.
 
 // Try playing computer with the example to work out what is going on
 
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5;
+//   const percentage = `${decimalNumber * 100}%`;
+
+//   return percentage;
+// }
+
+// console.log(decimalNumber);
+
+// decimalNumber in console.log isn't a defined number in the functions parameter now so it will return back as NaN "Not a Number", by adding '0.05'  to the console.log it now has a number to calculate the percentage to
+
+// Finally, correct the code to fix the problem
+
+
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
+
 }
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+  console.log(convertToPercentage(0.5))
